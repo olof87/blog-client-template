c10ee56bf7e3426467bc06949dcee66e106b934b
Olof Stigsson Flygare <olofstigssonflygare@gmail.com>
2023-12-14 15:20:14 +0100

    ändringar

---
+++ js/posts.js
@@ -0,0 +1,50 @@
+async function fetchAllPosts() {
+    try {
+        const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
+        const posts = await response.json();
+
+        let postListHTML = "";
+        for (let post of posts) {
+            postListHTML += `
+                <li class="list-group-item">
+                    ${post.title} <br>Tags: ${post.tags} <br><br>
+                    ${post.author} <br>
+                    <p> ${post.content} <br> <span class="date">-- ${post.date} </span> <p>
+
+                    
+                    <a href="#">Read more</a> <br>
+                    </li>
+                    <br>
+                    <br>
+
+            `
+        }
+
+        document.getElementById('post-list').innerHTML = postListHTML;
+
+    } catch (error){
+        console.log(error)
+    }
+}
+
+fetchAllPosts();
+
+
+/*
+postListHTML += `
+                <li class="list-group-item">
+                    ${post.title} <br>Tags: ${post.tags} <br><br>
+                    ${post.author} <br>
+                    <p> ${post.content} <br> <span class="date">-- ${post.date} </span> <p>
+
+                    
+                    <a href="#">Read more</a> <br>
+                    </li>
+                    <br>
+                    <br>
+
+            `
+
+
+
+*/
